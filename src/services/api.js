7d3d84b06933701d870970e095f4bc83febe7595
Ianqrzz <ianepeu22@gmail.com>
2026-07-02 19:58:09 -0300

    Fix: Correct baseURL format in API service configuration

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-  baseURL: 'https://prg04ianlucasqueirozbackend-production.up.railway.app'
+  baseURL: 'prg04ianlucasqueiroz-backend-production.up.railway.app'
 })
 
 
